5ba41b3a353322fa598a7fa19335dda1d6a45d58
cagatayasar <cagatayasar@hotmail.com>
2023-04-16 18:41:41 +0300

    moved utils from the upper project

--- CombatUtils.cs
+++ CombatUtils.cs
@@ -2,11 +2,49 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Text;
 
 public static partial class Utils
 {
     public static NumberFormatInfo decimalSeperator = new NumberFormatInfo();
     public static float Rad2Deg => 180f / MathF.PI;
+
+    public static float NextFloat(this System.Random rnd, float minValue = 0f, float maxValue = 1f)
+    {
+        var randomDbl = rnd.NextDouble();
+        return (float) (randomDbl * ((double) maxValue - (double) minValue) + minValue);
+    }
+
+    public static bool NextBool(this System.Random rnd)
+    {
+        return rnd.Next() > (Int32.MaxValue / 2);
+    }
+
+    public static IList<T> Shuffle<T>(this IList<T> list, ref System.Random rnd)
+    {
+        int n = list.Count;
+        while (n > 1) {
+            n--;
+            int k = rnd.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+        return list;
+    }
+
+    public static string ToString(this List<List<CW>> rowsList)
+    {
+        var sb = new System.Text.StringBuilder();
+        for (int i = 0; i < rowsList.Count; i++)
+        {
+            sb.Append($"Row {i+1}: ");
+            foreach (var cw in rowsList[i])
+                sb.Append($"{cw.weapon.weaponType}".PadRight(15, ' ').Substring(0, 15));
+            sb.Append($"\n");
+        }
+        return sb.ToString();
+    }
 
     public static R To<T, R>(this T item, Func<T, R> func)
     {
@@ -101,5 +139,41 @@ public static partial class Utils
             }
         }
         return null;
+    }
+
+    public static string ReplaceVariable(this string desc, object obj, char delimeterBegin, char delimeterEnd, out bool success, Func<string, string> actOnVariableName = null)
+    {
+        success = false;
+        if (string.IsNullOrEmpty(desc)) return desc;
+
+        var startIndex = desc.IndexOf(delimeterBegin, 0);
+        if (startIndex == -1) return desc;
+
+        var endIndex = desc.IndexOf(delimeterEnd, startIndex);
+        if (endIndex == -1) return desc;
+
+        var sb = new StringBuilder();
+        var variableName = desc.Substring(startIndex + 1, endIndex - startIndex - 1);
+        if (actOnVariableName != null)
+            variableName = actOnVariableName(variableName);
+
+        sb.Append(desc.Substring(0, startIndex));
+        sb.Append(obj.GetVariable(variableName));
+        sb.Append(desc.Substring(endIndex + 1, desc.Length - endIndex - 1));
+
+        success = true;
+        return sb.ToString();
+    }
+
+    public static string GetVariable(this object obj, string variable)
+    {
+        var fieldType = obj.GetType().GetField(variable).FieldType;
+        var value = obj.GetType().GetField(variable).GetValue(obj);
+
+        if (fieldType == typeof(float)) {
+            return Utils.GetWithDecimalZero((float) value);
+        } else {
+            return value.ToString();
+        }
     }
 }
