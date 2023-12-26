deaac8fa1dbd05f15d86a06cf36055da339eabae
cagatayasar <cagatayasar@hotmail.com>
2023-12-26 19:08:31 +0300

    changed LogSpaced param to array of objects

--- ILogger.cs
+++ ILogger.cs
@@ -4,7 +4,7 @@ public interface ILogger
 {
     void Log(string value);
     void LogError(string value);
-    void LogSpaced(string value);
-    void LogTabbed(params object[] value);
+    void LogSpaced(params object[] values);
+    void LogTabbed(params object[] values);
 }
 }
